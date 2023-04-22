d2f32beb34b15fd57b583250f1d0365600e0d61d
jakobhindemith <119002658+jakobhindemith@users.noreply.github.com>
2023-04-22 17:45:50 +0200

    Add files via upload

---
+++ Kategorien.java
@@ -0,0 +1,19 @@
+package kalender;
+
+/**
+ * Klasse welche die Verschiedenen Ereigniss 
+ * Kategorien definiert
+ * 
+ * @author jakobhindemith
+ *
+ */
+
+public class Kategorien {
+	
+	public void EreignissKategorie() {
+		
+		
+		
+	}
+		
+}
